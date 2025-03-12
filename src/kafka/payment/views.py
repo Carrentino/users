ff4ece40e8e4840dab4fd7e61252e53e3666ed33
Михаил <mxoilslukin@gmail.com>
2025-03-13 01:59:14 +0500

    fixes

--- src/kafka/payment/views.py
+++ src/kafka/payment/views.py
@@ -2,7 +2,7 @@ from helpers.depends.db_session import get_db_session_context
 from helpers.kafka.consumer import KafkaConsumerTopicsListeners
 
 from src.kafka.db_client import make_db_client
-from src.kafka.depends import get_user_service
+from src.kafka.depends.services import get_user_service
 from src.kafka.payment.schemas import UpdateBalance
 from src.settings import get_settings
 
